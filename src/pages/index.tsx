88a6bc4c93fb6f2153b216af703936a9147348a3
Edwin Montealvo <edmdc@Edwins-MacBook-Pro.local>
2020-12-24 16:16:05 -0600

    Delete gatsby boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const IndexPage = (): JSX.Element => {
+  return (
+    <main>
+      <div className="banner">
+        <h1>ReRent</h1>
+        <h2>Home-sharing Made Easy</h2>
+      </div>
+    </main>
+  )
+}
+
+export default IndexPage
