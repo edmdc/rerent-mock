1dadd3377236ef03cf9d675eedeecfda9dd60f05
Edwin Montealvo <edmdc@Edwins-MacBook-Pro.local>
2020-12-25 16:05:20 -0600

    Add base heading styles

---
+++ src/components/common/headings.tsx
@@ -0,0 +1,25 @@
+import tw from 'twin.macro';
+
+export const H1 = tw.h1`
+  text-8xl my-3 font-bold
+`;
+
+export const H2 = tw.h2`
+  text-6xl my-3 font-bold
+`;
+
+export const H3 = tw.h3`
+  text-4xl my-3 font-bold
+`;
+
+export const H4 = tw.h4`
+  text-2xl my-3 font-bold 
+`;
+
+export const H5 = tw.h5`
+  text-lg my-3 font-bold
+`;
+
+export const H6 = tw.h6`
+  text-base my-3 font-bold
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
 import AboutSlide from '../components/AboutSlide';
+import { H1, H3 } from '../components/common/headings';
 import tw from 'twin.macro';
 import livingRoom from '../images/rerent-living-room.svg';
 import diningRoom from '../images/dining-room.svg';
@@ -8,14 +9,6 @@ import diningRoom from '../images/dining-room.svg';
 const Banner = tw.div`
   h-96 bg-primary flex flex-col content-center font-heading
   justify-center text-center text-white
-`;
-
-const H1 = tw.h1`
-  text-8xl my-3
-`;
-
-const H3 = tw.h3`
-  text-5xl max-w-3xl self-center my-3
 `;
 
 // markup
