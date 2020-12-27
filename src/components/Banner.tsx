2d4c4676fb5cf88060a8ac5c10e69524b1654b39
Edwin Montealvo <edmdc@Edwins-MacBook-Pro.local>
2020-12-26 18:17:10 -0600

    Add banner buttton and project documentation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,55 +1,48 @@
 /* @jsx jsx */
-import { jsx, css, keyframes } from '@emotion/react';
+import { jsx, css } from '@emotion/react';
 import tw, { styled } from 'twin.macro';
+import { fadeIn } from '../styles/keyframes';
 import { H1, H3 } from '../components/common/headings';
 
+// Twin.macro parses tailwind class names into styles for
+// use within CSS-in-JS. Here's the Basic Syntax:
+const SimpleButton = tw.button`bg-transparent rounded border
+  py-2 px-3 mt-8 font-heading hover:bg-white hover:text-primary
+`;
+
+// Twin.macro also allows for quick Tailwind overrides by
+// extending abstraction provided by @emotion, like so:
 const Container = styled.div`
   ${tw`bg-primary font-heading text-center text-white z-0 pt-20`}
   height: 30rem;
 `;
 
-const fadeInTop = keyframes`
-  from {
-    transform: translateY(-100%);
-    opacity: 0;
-    tranform-origin: top;
-  }
-
-  100% {
-    transform: translateY(0%);
-    opacity: 1;
-  }
-`;
-
-const fadeInBottom = keyframes`
-  from {
-    transform: translateY(100%);
-    opacity: 0;
-    tranform-origin: bottom;
-  }
-
-  100% {
-    transform: translateY(0%);
-    opacity: 1;
-  }
-`;
-
+// A css prop is added to components by @emotion's jsx pragma
+// (included by comment + first import on top of file)
+// Here @emotion css utility is used to add animations to components.
 const Banner = (): JSX.Element => (
   <Container>
     <H1
       css={css`
-        animation: ${fadeInTop} 1s ease-in-out;
+        animation: ${fadeIn.top} 1s ease-in-out;
       `}
     >
       ReRent
     </H1>
     <H3
       css={css`
-        animation: ${fadeInBottom} 1s ease-in-out;
+        animation: ${fadeIn.bottom} 1s ease-in-out;
       `}
     >
       Home-sharing Made Easy
     </H3>
+    <SimpleButton
+      css={css`
+        animation: ${fadeIn.simple} 1.2s ease-in;
+      `}
+    >
+      Join Now
+    </SimpleButton>
   </Container>
 );
 
